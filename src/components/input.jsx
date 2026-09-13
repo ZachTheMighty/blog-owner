21d6da0ba0ef613e8d5f16ac28796d3e82a12f29
ZachTheMighty <promoted66@gmail.com>
2026-09-13 05:33:22 +0300

    feat: sign up form now creates a user in database

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,11 +1,19 @@
-export default function Input({ type, text = type }) {
+export default function Input({
+  type,
+  text = type,
+  name = type,
+  value,
+  setValue,
+}) {
   return (
     <div className="flex flex-col justify-center">
-      <label htmlFor={text}>{text[0].toUpperCase() + text.slice(1)}:</label>
+      <label htmlFor={name}>{text[0].toUpperCase() + text.slice(1)}:</label>
       <input
         type={type}
-        name={text}
-        id={text}
+        name={name}
+        id={name}
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
         className="border-1 border-black/10 p-5 mb-3 hover:outline-1 outline-cyan-600 focus:outline-2 outline-cyan-600 sm:w-120"
         placeholder={`Enter ${text}`}
       />
--- src/components/sign_up.jsx
+++ src/components/sign_up.jsx
@@ -1,15 +1,50 @@
 import Input from "./input.jsx";
 import Form from "./form.jsx";
+import { useState } from "react";
 
 export default function Signup() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
   return (
     <div className="min-h-screen flex justify-center px-8 items-center bg-cyan-600">
-      <Form header="Sign Up" button="SIGN UP">
-        <Input type="text" text="First Name" />
-        <Input type="text" text="Last Name" />
-        <Input type="email" />
-        <Input type="password" />
-        <Input type="password" text="Confirm Password" />
+      <Form
+        header="Sign Up"
+        button="SIGN UP"
+        fields={{
+          firstName,
+          lastName,
+          email,
+          password,
+          confirmPassword,
+        }}
+      >
+        <Input
+          type="text"
+          text="First Name"
+          name="firstName"
+          value={firstName}
+          setValue={setFirstName}
+        />
+        <Input
+          type="text"
+          text="Last Name"
+          name="lastName"
+          value={lastName}
+          setValue={setLastName}
+        />
+        <Input type="email" value={email} setValue={setEmail} />
+        <Input type="password" value={password} setValue={setPassword} />
+        <Input
+          type="password"
+          text="Confirm Password"
+          name="confirmPassword"
+          value={confirmPassword}
+          setValue={setConfirmPassword}
+        />
       </Form>
     </div>
   );
