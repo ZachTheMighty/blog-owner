c12165d6701bb1f9ed21c229d10b0e1952f4c21d
ZachTheMighty <promoted66@gmail.com>
2026-09-15 11:33:29 +0300

    feat: username in navbar now links to dashboard

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -5,9 +5,12 @@ import { Link } from "react-router";
 export default function Navbar({ userName }) {
   return (
     <div className="sm:flex sm:justify-between sm:items-center">
-      <div className="text-xl sm:text-3xl font-bold">
+      <Link
+        to="/dashboard"
+        className="text-xl sm:text-2xl font-bold lg:text-3xl hover:text-pink-500 active:text-pink-600"
+      >
         Welcome back, {userName}
-      </div>
+      </Link>
       <div className="flex justify-center items-center mt-8 gap-8 sm:m-0">
         <div className="flex gap-2 hover:bg-gray-200 px-4 py-2 rounded-full active:bg-gray-300">
           <Plus />
