ee0a41fbe7ff043f5e825fdba6e9932c4cd5f50f
ZachTheMighty <promoted66@gmail.com>
2026-09-15 11:35:10 +0300

    fix: Create post link only clickable on the Create text not the whole box

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -12,10 +12,13 @@ export default function Navbar({ userName }) {
         Welcome back, {userName}
       </Link>
       <div className="flex justify-center items-center mt-8 gap-8 sm:m-0">
-        <div className="flex gap-2 hover:bg-gray-200 px-4 py-2 rounded-full active:bg-gray-300">
+        <Link
+          to="posts/new"
+          className="flex gap-2 hover:bg-gray-200 px-4 py-2 rounded-full active:bg-gray-300"
+        >
           <Plus />
-          <Link to="posts/new">Create</Link>
-        </div>
+          <div>Create</div>
+        </Link>
         <div className="p-2 hover:bg-gray-200 active:bg-gray-300 rounded-full">
           <UserRound />
         </div>
