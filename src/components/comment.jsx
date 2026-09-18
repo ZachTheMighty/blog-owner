032fb56006998cc2c00a44313b5ab3df55369ec6
ZachTheMighty <promoted66@gmail.com>
2026-09-18 17:31:40 +0300

    fix: incorrect endpoints

--- src/components/comment.jsx
+++ src/components/comment.jsx
@@ -8,7 +8,7 @@ export default function Comment({ comment }) {
 
   const handleDelete = async () => {
     const response = await fetch(
-      `https://blog-api-ljzu.onrender.com/${id}/comments/${comment.id}`,
+      `https://blog-api-ljzu.onrender.com/posts/${id}/comments/${comment.id}`,
       {
         method: "delete",
       },
--- src/components/post.jsx
+++ src/components/post.jsx
@@ -9,7 +9,7 @@ export default function Post({ post, full }) {
 
   const handleViewPost = async () => {
     if (full) return;
-    await fetch(`https://blog-api-ljzu.onrender.com/${post.id}/views`, {
+    await fetch(`https://blog-api-ljzu.onrender.com/posts/${post.id}/views`, {
       method: "post",
     })
       .then((response) => response.json())
@@ -48,7 +48,7 @@ export default function Post({ post, full }) {
 
   const handleDelete = async (event) => {
     event.stopPropagation();
-    await fetch(`https://blog-api-ljzu.onrender.com/${post.id}`, {
+    await fetch(`https://blog-api-ljzu.onrender.com/posts/${post.id}`, {
       method: "delete",
     })
       .then(() => setPosts(posts.filter((item) => item.id !== post.id)))
